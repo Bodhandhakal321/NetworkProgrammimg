72a76bfcf7425ba6cea94a6fbe13662aa7a377d8
Bodhan Dhakal <bodhandhakal22@gmail.com>
2024-07-12 07:49:47 +0545

    byte buffer properties

--- src/Chapter9/Chapter9.java
+++ src/Chapter9/Chapter9.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
+import java.nio.*;
 import java.nio.channels.Channels;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
@@ -52,6 +51,42 @@ public class Chapter9 {
 
             //mark
             System.out.println( "mark is" +byteBuffer.mark());
+
+            //clear
+            System.out.println("is clear" + byteBuffer.clear());
+
+            //flip
+            System.out.println( "FLIPPED " + byteBuffer.flip());
+
+            //rewind
+            System.out.println("REWIND" +byteBuffer.rewind());
+
+            //remaining
+            System. out.println("Number of remaining"+ byteBuffer.remaining());
+
+            //hasRemaining
+            System.out.println(byteBuffer.hasRemaining());
+
+            //allocating different buffers
+            IntBuffer intBuffer = IntBuffer.allocate(74);
+            FloatBuffer floatBuffer = FloatBuffer.allocate(74);
+            DoubleBuffer doubleBuffer = DoubleBuffer.allocate(74);
+
+            //rewriting buffer into arrays
+            int[] array1 = intBuffer.array();
+            float[] array2 = floatBuffer.array();
+            double[] array3 =  doubleBuffer.array();
+
+             ByteBuffer byteBuffer1 = ByteBuffer.allocateDirect(74);
+
+            //redirect isnot supported in direct allocations
+//             byte[] array4 = byteBuffer1.array();
+
+            char[] text = "Some text".toCharArray();
+            CharBuffer charBuffer=  CharBuffer.wrap(text);
+
+            CharBuffer charBuffer1 = CharBuffer.allocate(10);
+
 
 
 
